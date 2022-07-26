06e7c3d7fe62dfc286439509ed29653a0e472b63
FIRST_NAME LAST_NAME <jgabrielsencioles@gmail.com>
2022-07-26 10:01:58 -0300

    update heroes

--- src/app/pages/naruto/naruto-home/naruto-home.component.ts
+++ src/app/pages/naruto/naruto-home/naruto-home.component.ts
@@ -6,7 +6,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NarutoHomeComponent implements OnInit {
   constructor() {}
-
+  Naruto = [
+    {
+      name: 'Naruto Uzumaki',
+      photoName: 'naruto-uzumaki',
+      text: 'Sem pais e sozinho naruto é o melhor ninja',
+      estilos: [{ estilo: 'ar' }, { estilo: 'agua' }],
+    },
+    {
+      name: 'Sakura Haruno',
+      photoName: 'sakura-haruno',
+      text: 'Sem pais e sozinho naruto é o melhor ninja',
+      estilos: [{ estilo: 'ar' }, { estilo: 'agua' }],
+    },
+    {
+      name: 'Sasuke Uchiha',
+      photoName: 'sasuke-uchiha',
+      text: 'Sem pais e sozinho naruto é o melhor ninja',
+      estilos: [{ estilo: 'ar' }, { estilo: 'agua' }],
+    },
+    {
+      name: 'Naruto Uzumaki',
+      photoName: 'naruto-uzumaki',
+      text: 'Sem pais e sozinho naruto é o melhor ninja',
+      estilos: [{ estilo: 'ar' }, { estilo: 'agua' }],
+    },
+    {
+      name: 'Naruto Uzumaki',
+      photoName: 'naruto-uzumaki',
+      text: 'Sem pais e sozinho naruto é o melhor ninja',
+      estilos: [{ estilo: 'ar' }, { estilo: 'agua' }],
+    },
+    {
+      name: 'Naruto Uzumaki',
+      photoName: 'naruto-uzumaki',
+      text: 'Sem pais e sozinho naruto é o melhor ninja',
+      estilos: [{ estilo: 'ar' }, { estilo: 'agua' }],
+    },
+    {
+      name: 'Naruto Uzumaki',
+      photoName: 'naruto-uzumaki',
+      text: 'Sem pais e sozinho naruto é o melhor ninja',
+      estilos: [{ estilo: 'ar' }, { estilo: 'agua' }],
+    },
+    {
+      name: 'Naruto Uzumaki',
+      photoName: 'naruto-uzumaki',
+      text: 'Sem pais e sozinho naruto é o melhor ninja',
+      estilos: [{ estilo: 'ar' }, { estilo: 'agua' }],
+    },
+  ];
   ngOnInit(): void {
     console.log('inicio');
   }
